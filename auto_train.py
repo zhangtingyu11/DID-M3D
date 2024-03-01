0843408241476cb1cf52dd0d5a10bf6080b3c5bf
zhangtingyu11 <18013933973@163.com>
2024-03-01 09:24:52 +0800

    增加注意力机制

--- auto_train.py
+++ auto_train.py
@@ -50,7 +50,7 @@ def send_email(easy, mod, hard, map, res):
     password = "SNOYAHKUJNPWATEF"
 
     # 邮件内容
-    subject = "使用LRRU的深度补全结果(去掉<2.0的深度, 深度小于2的不预测"
+    subject = "使用LRRU的深度补全结果(去掉<2.0的深度, 另外一种attention"
     if res == 0:
         body = "当前各个难度的AP为({}, {}, {}), mAP为{}".format(easy, mod, hard, map)
     else:
@@ -71,10 +71,10 @@ def send_email(easy, mod, hard, map, res):
         server.sendmail(sender_email, receiver_email, message.as_string())
 
 if __name__ == "__main__":
-    for idx in range(1):
+    for idx in range(100):
         change_yaml_name('/home/public/zty/Project/DeepLearningProject/DID-M3D/config/kitti_car.yaml', idx)
         res = os.system('CUDA_VISIBLE_DEVICES=0,1 python tools/train_val.py --config config/kitti_car.yaml')
-        log_filename = '/home/public/zty/Project/DeepLearningProject/DID-M3D/work_dirs/kitti_models/logs/only_car_lrru_record_{}/train.log'.format(idx)
+        log_filename = '/home/public/zty/Project/DeepLearningProject/DID-M3D/work_dirs/kitti_models/logs/only_car_lrru_clip_transformer_encoder_another_attention_{}/train.log'.format(idx)
         if res == 0:
             easy, mod, hard = get_best_3dmod_acc(log_filename)
         else:
